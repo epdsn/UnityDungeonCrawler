bd6e7f92be6253bfa5b8cb394fa3562758e0b9e2
epdsn <ibestudios@gmail.com>
2025-12-27 18:17:28 -0700

    added jump and refatored some code

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -5,6 +5,7 @@ public class Player : MonoBehaviour
     private Rigidbody2D rb;
 
     [SerializeField] private float moveSpeed = 3.5f;
+    [SerializeField] private float jumpForce = 8;
     private float xInput;
 
     private void Awake()
@@ -14,8 +15,28 @@ public class Player : MonoBehaviour
 
     private void Update()
     {
+        HandleInput();
+        HandleMovement();
+    }
+
+    private void HandleInput()
+    {
         xInput = Input.GetAxisRaw("Horizontal");
+        HandleMovement();
+
+        if (Input.GetKeyDown(KeyCode.Space))
+            Jump();
+    }
+
+    private void HandleMovement()
+    {
         rb.linearVelocity = new Vector2(xInput * moveSpeed, rb.linearVelocity.y);
+    }
+
+    private void Jump()
+    {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+
     }
 
 }
