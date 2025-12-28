293edfaa593b7a4e401430b2e4a2e8596170e403
epdsn <ibestudios@gmail.com>
2025-12-28 12:13:43 -0700

    jump animation idle transition

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -35,11 +35,9 @@ public class Player : MonoBehaviour
 
     private void HandleAnimations()
     {
-       bool isMoving = rb.linearVelocity.x != 0;
-
-        anim.SetBool("isMoving", isMoving);
-
+        anim.SetFloat("xVelocity", rb.linearVelocity.x);
         anim.SetFloat("yVelocity", rb.linearVelocity.y);
+        anim.SetBool("isGrounded", isGrounded);
     }
 
     private void HandleInput()
