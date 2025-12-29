942a73b6b31fe0a79a1044381d79d9026a7e0d0d
epdsn <ibestudios@gmail.com>
2025-12-29 15:16:47 -0700

    color shift when enemy is hit

--- Assets/Enemy.cs
+++ Assets/Enemy.cs
@@ -1,9 +1,41 @@
+using System;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    private SpriteRenderer sr;
+    // Cooldown duration for red color effect
+    [SerializeField] private float redColorDuration = 1;
+
+    public float currentTimeInGame;
+    public float timeWhenLastHit;
+
+    private void Awake()
+    {
+        sr = GetComponent<SpriteRenderer>();
+    }
+
+    private void Update()
+    {
+        ChangeColorIfHit();
+    }
+
+    private void ChangeColorIfHit()
+    {
+        currentTimeInGame = Time.time;
+
+        if (currentTimeInGame > timeWhenLastHit + redColorDuration)
+        {
+            if (sr.color != Color.white)
+                sr.color = Color.white;
+        }
+
+    }
+
     public void TakeDamage()
     { 
-        Debug.Log( gameObject.name + " took damage!");
+        sr.color = Color.red;
+        timeWhenLastHit = Time.time;
     }
+
 }
